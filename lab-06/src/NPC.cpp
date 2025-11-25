809f9ca234617fc69a0a299e211e465821e1facf
Nasy <anastasiia.fimina@gmail.com>
2025-11-25 20:30:35 +0300

    Delete lab-06 directory

--- lab-06/src/NPC.cpp
+++
@@ -1,6 +0,0 @@
-#include "NPC.hpp"
-#include "Visitor.hpp"
-
-void Squirrel::accept(Visitor& v) { v.visit(*this); }
-void Elf::accept(Visitor& v) { v.visit(*this); }
-void Bandit::accept(Visitor& v) { v.visit(*this); }

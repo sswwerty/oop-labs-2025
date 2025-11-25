cb5958d58b881d882bc0e2623e29245711574f8f
Nasy <anastasiia.fimina@gmail.com>
2025-11-25 20:38:13 +0300

    Delete lab-06/build directory

--- lab-06/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anastasiafimina/Desktop/ВУЗ/oop-labs-2025/lab-06/src
-# Build directory: /Users/anastasiafimina/Desktop/ВУЗ/oop-labs-2025/lab-06/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
